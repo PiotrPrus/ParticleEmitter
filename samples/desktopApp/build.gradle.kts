8eb9eca5809e14ee8eca353bf0de06ef642912fd
Márton Braun <marton.braun@jetbrains.com>
2026-05-29 21:47:24 +0200

    Make desktop app a pure JVM project

--- samples/desktopApp/build.gradle.kts
+++ samples/desktopApp/build.gradle.kts
@@ -2,24 +2,25 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.compose)
 }
 
 kotlin {
-    jvm {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
+}
 
-    sourceSets {
-        jvmMain.dependencies {
-            implementation(project(":samples:shared"))
-            implementation(compose.desktop.currentOs)
-        }
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(project(":samples:shared"))
+    implementation(compose.desktop.currentOs)
 }
 
 compose.desktop {
